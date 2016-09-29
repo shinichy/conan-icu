5c6b44b8bae353a2a930b24de1edad283617b51f
Shinichi Katayama <shin1.katayama@gmail.com>
2016-09-29 18:49:04 +0900

    Enable x86 MacOS build

--- build.py
+++ build.py
@@ -4,9 +4,4 @@ import platform
 if __name__ == "__main__":
   builder = ConanMultiPackager( username="shinichy", channel="testing" )
   builder.add_common_builds( pure_c=False )
-  filtered_builds = []
-  for settings, options in builder.builds:
-    if platform.system() == "Darwin" and settings["arch"] in ["x86_64"]:
-      filtered_builds.append([settings, options])
-  builder.builds = filtered_builds
   builder.run()
